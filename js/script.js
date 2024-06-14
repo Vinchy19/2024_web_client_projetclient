8f3a289372a9f3d2e56d131cf769002cd2e79e79
vinchy19 <cherenfanyvinchy@gmail.com>
2024-06-14 10:50:39 -0400

    modifiction toggle

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const cicon = document.getElementById('cicon')
+cicon.addEventListener('click', () => toggleMenu())
+
+function toggleMenu() {
+    var Links = document.getElementById('links');
+    var cicon = document.querySelector('.icon i');
+
+    if (Links.classList.contains('linkshow'))
+        {
+        Links.classList.remove('linkshow');
+        cicon.classList.remove('fa-times');
+        cicon.classList.add('fa-bars');
+    } 
+    else
+    {
+        Links.classList.add('linkshow');
+        cicon.classList.remove('fa-bars');
+        cicon.classList.add('fa-times');
+    }
+}
